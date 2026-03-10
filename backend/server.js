ce0d090d57fbba44137230ba9c84faac10ae313f
Lavanya-Rachamadugu <laavanyarachamdugu@gmail.com>
2026-03-10 22:03:58 +0530

    fixed render port issue

--- backend/server.js
+++ backend/server.js
@@ -132,7 +132,7 @@ User Answer: ${userAnswer || "No answer provided"}`,
 /* =========================================================
    START SERVER
 ========================================================= */
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
